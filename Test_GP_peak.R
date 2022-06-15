ecb885f4677ed5c0548e2f41b894928d2cdf86cd
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2022-06-15 11:06:21 +0200

    Med hjälp av pracma, ska nu hitta toppar.

--- Test_GP_peak.R
+++ Test_GP_peak.R
@@ -1,15 +1,25 @@
-## setwd("D:/2021 Isaac/220614 Pollenrör")
-datafile = "CSV_Copy_2022_05_18_GP_data_pollenror.csv"
-pollen_dataframe = read.table(datafile, header = TRUE, sep = ";")
+## Setup with working directory and filenames 
+  setwd("C:/Users/isaac/Documents/Universitetet/Parmryd et al/Statistik/220614_Pollenror")
+  datafile = "E1_Copy_2022_05_18_GP_data_pollenror.csv"
+  pollen_dataframe = read.table(datafile, header = TRUE, sep = ",")
+  pollen_dataframe
 
-##väljer ut Avstånd och GP för en hel sida
-utvald_data = pollen_dataframe [1:294, c("Avstand", "GP")]
-utvald_data
+##valjer ut Avstand och GP for en hel sida
+  utvald_data = pollen_dataframe [1:200, c("Avstand", "GP")]
+  utvald_data
+  avstand_100 = pollen_dataframe [1:200, "Avstand"]
+  GP_100 = pollen_dataframe [1:200, "GP"]
 
-## Försök till att plotta lite
-library(tidyverse)
-plot(utvald_data)
+## Forsok till att plotta lite
+  require(stats) # for lowess, rpois, rnorm
+  require(graphics) # for plot methods
+  plot(utvald_data)
 
-p <- ggplot(utvald_data, aes(avstand, GP))
-p
-p + geom_point()
+## Try to find peaks
+  library(pracma)
+  nups = 6
+  findpeaks(GP_100, nups = 6, ndowns = nups, zero = "0", peakpat = NULL,
+            minpeakheight = -Inf, minpeakdistance = 1,
+            threshold = 0, npeaks = 0, sortstr = FALSE)
+  # 
+  
