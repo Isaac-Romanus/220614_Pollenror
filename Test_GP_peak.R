5cfa3865abf0946a7b89d0bdfc78b04f07d51301
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2022-06-15 11:11:26 +0200

    Commenting and cleaning

--- Test_GP_peak.R
+++ Test_GP_peak.R
@@ -1,25 +1,24 @@
-## Setup with working directory and filenames 
+## Setup with working directory and file names 
   setwd("C:/Users/isaac/Documents/Universitetet/Parmryd et al/Statistik/220614_Pollenror")
   datafile = "E1_Copy_2022_05_18_GP_data_pollenror.csv"
   pollen_dataframe = read.table(datafile, header = TRUE, sep = ",")
-  pollen_dataframe
 
-##valjer ut Avstand och GP for en hel sida
+## Selects "Avstand" and "GP-value" for one side of the pollentube
   utvald_data = pollen_dataframe [1:200, c("Avstand", "GP")]
-  utvald_data
   avstand_100 = pollen_dataframe [1:200, "Avstand"]
-  GP_100 = pollen_dataframe [1:200, "GP"]
+  GP_200 = pollen_dataframe [1:200, "GP"]
 
-## Forsok till att plotta lite
-  require(stats) # for lowess, rpois, rnorm
-  require(graphics) # for plot methods
+## Plotting the 200 / first GP values for 0.5 / 1px
+  require(stats) 
+  require(graphics) 
   plot(utvald_data)
 
 ## Try to find peaks
   library(pracma)
-  nups = 6
-  findpeaks(GP_100, nups = 6, ndowns = nups, zero = "0", peakpat = NULL,
+  nups = 6 #How many steps needed to count as a peak
+  findpeaks(GP_200, nups = 6, ndowns = nups, zero = "0", peakpat = NULL,
             minpeakheight = -Inf, minpeakdistance = 1,
             threshold = 0, npeaks = 0, sortstr = FALSE)
-  # 
+
+print("done")
   
