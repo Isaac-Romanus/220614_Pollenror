378f107f7a16cfbeece924b4026dcf00399cc4e3
Isaac-Romanus <Isaac.romanus.thiria@gu.se>
2022-06-17 13:41:10 +0200

    Test på större dataset, fortfarande ej helt funktionell

--- Test_GP_peak.R
+++ Test_GP_peak.R
@@ -1,5 +1,5 @@
-## Setup with working directory and file names 
-  setwd("C:/Users/isaac/Documents/Universitetet/Parmryd et al/Statistik/220614_Pollenror")
+##  Setup with working directory and file names 
+##  setwd("C:/Users/isaac/Documents/Universitetet/Parmryd et al/Statistik/220614_Pollenror")
   datafile = "E1_Copy_2022_05_18_GP_data_pollenror.csv"
   pollen_dataframe = read.table(datafile, header = TRUE, sep = ",")
   pollen_dataframe
@@ -7,7 +7,7 @@
   
 ##  A loop for going through the pollentubes
   i = 1
-  for (i in 1:300) {
+  for (i in 1:21000) {
     if (
       (pollen_dataframe[i, "Avstand"] +  
        pollen_dataframe[(i + 1) , "Avstand"]) 
@@ -71,21 +71,21 @@
   
 
 ## Selects "Avstand" and "GP-value" for one side of the pollentube
-  utvald_data = pollen_dataframe [1:200, c("Avstand", "GP")]
-  avstand_100 = pollen_dataframe [1:200, "Avstand"]
-  GP_200 = pollen_dataframe [1:200, "GP"]
+  # utvald_data = pollen_dataframe [1:200, c("Avstand", "GP")]
+  # avstand_100 = pollen_dataframe [1:200, "Avstand"]
+  # GP_200 = pollen_dataframe [1:200, "GP"]
 
 ## Plotting the 200 / first GP values for 0.5 / 1px
-  require(stats) 
-  require(graphics) 
-  plot(utvald_data)
+  # require(stats) 
+  # require(graphics) 
+  # plot(utvald_data)
 
 ## Try to find peaks, col 1 = value, col 2 = peak avstand, col 3/4 = start/end
-  library(pracma)
-  nups = 10
-  findpeaks(GP_200, nups = 10, ndowns = nups, zero = "0", peakpat = NULL,
-            minpeakheight = -Inf, minpeakdistance = 1,
-            threshold = 0, npeaks = 0, sortstr = FALSE)
+  # library(pracma)
+  # nups = 10
+  # findpeaks(GP_200, nups = 10, ndowns = nups, zero = "0", peakpat = NULL,
+  #           minpeakheight = -Inf, minpeakdistance = 1,
+  #           threshold = 0, npeaks = 0, sortstr = FALSE)
   
 ##  Creating a function for selecting and plotting GP x avstånd
   plot_GP_avstand = function(from, to) {
